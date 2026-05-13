db051ea26ba1aed3437a5c873c1d0f85efbb448e
kkh <11kkh19999@gmail.com>
2026-05-14 00:13:18 +0900

    chore: move project to spring boot layout

---
+++ src/test/java/com/enjoytrip/common/controller/HomeControllerTest.java
@@ -0,0 +1,24 @@
+package com.enjoytrip.common.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void indexReturnsIndexView() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
